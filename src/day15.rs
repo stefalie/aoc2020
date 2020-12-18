296871cc12dc18b57f8c11a3bc60e2a15c054832
Stefan Lienhard <stefan.lienhard@virtamed.com>
2020-12-18 21:41:20 +0100

    day 15

---
+++ src/day15.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+
+fn play(numbers: &[i32], n: usize) -> i32 {
+    let mut last_turn: HashMap<i32, i32> = HashMap::new();
+
+    for (i, n) in numbers[..(numbers.len() - 1)].iter().enumerate() {
+        last_turn.insert(*n, i as i32  + 1);
+    }
+
+    let mut last: i32 = *numbers.iter().last().unwrap();
+    for i in (numbers.len() + 1)..=n {
+        let curr = match last_turn.get(&last) {
+            Some(t) => i as i32 - 1 - t,
+            None => 0,
+        };
+        last_turn.insert(last, i as i32 - 1);
+        last = curr;
+    }
+
+    return last;
+}
+
+pub fn run() {
+    let input_bytes = include_bytes!("day15_input");
+    let input_string = String::from_utf8(input_bytes.to_vec()).unwrap();
+
+    let numbers: Vec<i32> = input_string
+        .trim()
+        .split(',')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    part1(&numbers);
+    part2(&numbers);
+}
+
+fn part1(numbers: &[i32]) {
+    let result = play(numbers, 2020);
+    println!("Day 15, part 1: {}", result);
+}
+
+fn part2(numbers: &[i32]) {
+    let result = play(numbers, 30000000);
+    println!("Day 15, part 2: {}", result);
+}
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod day11;
 mod day12;
 mod day13;
 mod day14;
+mod day15;
 
 fn main() {
     day1::run();
@@ -28,4 +29,5 @@ fn main() {
     day12::run();
     day13::run();
     day14::run();
+    day15::run();
 }
